81845ee2fb065fd33fda20f217912fa04ac55be7
AlexRomanych <48482418+AlexRomanych@users.noreply.github.com>
2024-06-26 15:58:01 +0300

    commit initial

---
+++ one_hot_view.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import random
+import os
+
+os.system('cls')    # очистка консоли
+
+# избавляемся от "магических" чисел
+ROBOT_WORD  = 'robot'
+HUMAN_WORD  = 'human'
+COLUMN_NAME = 'whoAmI'
+ITEM_AMOUNT = 10
+
+lst =  [ROBOT_WORD] * ITEM_AMOUNT
+lst += [HUMAN_WORD] * ITEM_AMOUNT
+
+random.shuffle(lst)
+
+df = pd.DataFrame({COLUMN_NAME:lst})
+
+
+def sep_data():
+    '''
+        Отделяем данные
+    '''
+    print('', '_' * 68, '', sep='\n')
+
+
+if __name__ == '__main__':
+
+    print('________________________ Исходные данные: __________________________ ')
+    print(df)
+    sep_data()
+
+    print('_______________ One Hot View Without get_dummies(): ________________ ')
+
+    df.loc[df[COLUMN_NAME] == HUMAN_WORD, [HUMAN_WORD]] = (df[COLUMN_NAME] == HUMAN_WORD)
+    df.loc[df[COLUMN_NAME] == ROBOT_WORD, [HUMAN_WORD]] = (df[COLUMN_NAME] == HUMAN_WORD)
+
+    df.loc[df[COLUMN_NAME] == ROBOT_WORD, [ROBOT_WORD]] = (df[COLUMN_NAME] == ROBOT_WORD)
+    df.loc[df[COLUMN_NAME] == HUMAN_WORD, [ROBOT_WORD]] = (df[COLUMN_NAME] == ROBOT_WORD)
+
+    print(df[[HUMAN_WORD, ROBOT_WORD]])
+    sep_data()
+
+    print('___________________ Проверочка: pd.get_dummies() ___________________ ')
+    print(pd.get_dummies(df[COLUMN_NAME]))
+    sep_data()
